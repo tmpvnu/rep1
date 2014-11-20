7b8d16a555b062acc1d37e3c33c8e172cfa0e820
tmpvnu <tmpvnu@gmail.com>
2014-11-20 10:44:54 +0530

    server File new

--- demo.php
+++ demo.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "";
+$dbname = "myCart";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
